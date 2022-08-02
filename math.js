629c26a1a433c6532567be3345a609e0e945f01a
Yui Guji <nikitunzod@gmail.com>
2022-08-02 22:18:24 +0300

    Math functions added

---
+++ math.js
@@ -0,0 +1,15 @@
+function add(varA, varB) {
+    return varA + varB
+}
+
+function subtract(varA, varB) {
+    return varA - varB
+}
+
+function multiply(varA, varB) {
+    return varA * varB
+}
+
+function divide(varA, varB) {
+    return varA / varB
+}
