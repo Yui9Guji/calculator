3d11543a840d0a61f5ea717de4bea0e300ed5240
Yui Guji <nikitunzod@gmail.com>
2022-08-03 21:38:11 +0300

    Storing numbers in values

--- math.js
+++ math.js
@@ -35,24 +35,144 @@ function operate(operator, numA, numB) {
 }
 
 const buttons = document.querySelectorAll('button')
-const display = document.querySelector('.numbers')
+const display = document.querySelector('.screen')
+
+const numberOne = document.querySelector('.numberOne')
+const numberTwo = document.querySelector('.numberTwo')
+const addFunction = document.querySelector('.addFunction')
 
 buttons.forEach((button) => {
+
+    let counter = 0
 
     button.addEventListener('click', () => {
 
         const value = button.textContent
 
+
         if (value === 'AC') {
-            display.textContent = ''
+            numberOne.textContent = ''
+            numberTwo.textContent = ''
+            addFunction.textContent = ''
         }
 
-        else if (value === 'C') {
-            let displayValue = display.textContent
-            let array = displayValue.split('')
-            array.splice(array.length - 1, 1)
-            display.textContent = array.join('')
+        else if (value === '1') {
+
+            if (counter === 0) {
+                numberOne.textContent += 1
+            }
+            else {
+                numberTwo.textContent += 1
+            }
+
         }
+
+        else if (value === '2') {
+
+            if (counter === 0) {
+                numberOne.textContent += 2
+            }
+            else {
+                numberTwo.textContent += 2
+            }
+
+        }
+
+        else if (value === '3') {
+
+            if (counter === 0) {
+                numberOne.textContent += 3
+            }
+            else {
+                numberTwo.textContent += 3
+            }
+
+        }
+
+        else if (value === '4') {
+
+            if (counter === 0) {
+                numberOne.textContent += 4
+            }
+            else {
+                numberTwo.textContent += 4
+            }
+
+        }
+
+        else if (value === '5') {
+
+            if (counter === 0) {
+                numberOne.textContent += 5
+            }
+            else {
+                numberTwo.textContent += 5
+            }
+
+        }
+
+        else if (value === '6') {
+
+            if (counter === 0) {
+                numberOne.textContent += 6
+            }
+            else {
+                numberTwo.textContent += 6
+            }
+
+        }
+
+        else if (value === '7') {
+
+            if (counter === 0) {
+                numberOne.textContent += 7
+            }
+            else {
+                numberTwo.textContent += 7
+            }
+
+        }
+
+        else if (value === '8') {
+
+            if (counter === 0) {
+                numberOne.textContent += 8
+            }
+            else {
+                numberTwo.textContent += 8
+            }
+
+        }
+
+        else if (value === '9') {
+
+            if (counter === 0) {
+                numberOne.textContent += 9
+            }
+            else {
+                numberTwo.textContent += 9
+            }
+
+        }
+
+        else if (value === '0') {
+
+            if (counter === 0) {
+                numberOne.textContent += 0
+            }
+            else {
+                numberTwo.textContent += 0
+            }
+
+        }
+
+        else if (value === '+') {
+            counter++
+        }
+
+        console.log(numberOne)
+        console.log(numberTwo)
+        console.log(addFunction)
+
     })
-
 })
